9cc1bf9efed72dae8a2a8aacd230f1e255076519
Joshua Hecker <jhecker2001@gmail.com>
2023-11-16 11:24:48 -0600

    Added function to fill the Google Sheet with IATA codes.

--- data_manager.py
+++ data_manager.py
@@ -1,5 +1,6 @@
 import requests
 import os
+from flight_search import FlightSearch
 
 
 class DataManager:
@@ -8,14 +9,33 @@ class DataManager:
         self.header = {
             "Authorization": f"Basic {os.environ["SHEETY_AUTH"]}"
         }
-        self.get_endpoint = "https://api.sheety.co/e07399c5a2dc2b012ac72f6585597114/flightDeals/prices"
+        self.endpoint = "https://api.sheety.co/e07399c5a2dc2b012ac72f6585597114/flightDeals/prices"
 
     def get_rows(self):
         try:
-            response = requests.get(url=self.get_endpoint, headers=self.header)
+            response = requests.get(url=self.endpoint, headers=self.header)
             response.raise_for_status()
         except requests.HTTPError:
             pass
         else:
             data = response.json()
             return data
+
+    def fill_codes(self):
+        data = self.get_rows()["prices"]
+        searcher = FlightSearch()
+        for entry in data:
+            city = entry["city"]
+            row_id = entry["id"]
+            iata_code = searcher.get_iata(city)
+            put_endpoint = f"{self.endpoint}/{row_id}"
+            changes = {
+                "price": {
+                    "IATA Code": iata_code
+                }
+            }
+            try:
+                response = requests.put(url=put_endpoint, json=changes, headers=self.header)
+                response.raise_for_status()
+            except requests.HTTPError:
+                pass
--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 # This file will need to use the DataManager,FlightSearch, FlightData,
 # NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+
+data = DataManager()
+data.fill_codes()

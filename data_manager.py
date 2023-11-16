798943700f13480e4a55fee614946d883eaf6b45
Joshua Hecker <jhecker2001@gmail.com>
2023-11-16 10:54:51 -0600

    Added functions to get rows from a Google Sheets sheet and to get the IATA code of a city.

--- data_manager.py
+++ data_manager.py
@@ -6,7 +6,16 @@ class DataManager:
     # This class is responsible for talking to the Google Sheet.
     def __init__(self):
         self.header = {
-            "Authorization": os.environ["SHEETY_AUTH"]
+            "Authorization": f"Basic {os.environ["SHEETY_AUTH"]}"
         }
+        self.get_endpoint = "https://api.sheety.co/e07399c5a2dc2b012ac72f6585597114/flightDeals/prices"
 
-    pass
+    def get_rows(self):
+        try:
+            response = requests.get(url=self.get_endpoint, headers=self.header)
+            response.raise_for_status()
+        except requests.HTTPError:
+            pass
+        else:
+            data = response.json()
+            return data
--- flight_search.py
+++ flight_search.py
@@ -1,3 +1,26 @@
+import requests
+import os
+
+
 class FlightSearch:
     # This class is responsible for talking to the Flight Search API.
-    pass
+    def __init__(self):
+        self.apikey = os.environ["TEQUILA_KEY"]
+        self.endpoint = "https://api.tequila.kiwi.com/"
+
+    def get_iata(self, city):
+        search_endpoint = f"{self.endpoint}locations/query"
+        parameters = {
+            "apikey": self.apikey,
+            "term": city,
+            "locale": "en-US",
+            "location_types": "city"
+        }
+        try:
+            response = requests.get(url=search_endpoint, params=parameters)
+            response.raise_for_status()
+        except requests.HTTPError:
+            return "null"
+        else:
+            iata_code = response.json()["locations"][0]["code"]
+            return iata_code
